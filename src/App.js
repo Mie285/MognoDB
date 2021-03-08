7c22ff3f3534bbddab1c6e73377098259ebe3d23
worada <worada.wa@ku.th>
2021-03-08 13:25:45 +0700

    backend

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import Navbar from "./component/Navbar";
+import ExercisesList from "./component/ExercisesList";
+import EditExercise from "./component/EditExercise";
+import CreateExercise from "./component/CreateExercise";
+import CreateUser from "./component/CreateUser";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar />
+    <br />
+    <Route exact path="/" component = {ExercisesList} />
+    <Route path="/edit/:id" component = {EditExercise} />
+    <Route path="/create" component = {CreateExercise} />
+    <Route path="/user" component = {CreateUser} />
+    </BrowserRouter>
+  ); 
+}
+
+export default App;
